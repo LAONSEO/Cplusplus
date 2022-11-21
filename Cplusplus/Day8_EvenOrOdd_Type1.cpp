69b02070350293229239146c638e04f73b1bfb91
LAONSEO <gerard2725@gmail.com>
2022-11-22 00:31:09 +0900

    revision.

--- Cplusplus/Day8_EvenOrOdd_Type1.cpp
+++ Cplusplus/Day8_EvenOrOdd_Type1.cpp
@@ -3,23 +3,43 @@
 
 int main()
 {
-    int nInput{};
-    int nRemainder{};
+	int nInput{};
+	int nRemainder{};
 
-    printf("Enter a Number.\n");
-    scanf_s("%d", &nInput);
+	printf("Enter a Number.\n");
+	scanf_s("%d", &nInput);
 
-    nRemainder = nInput % 2;
+	nRemainder = nInput % 2;
 
-    if (nInput == 0)
-        printf("Unable to determine.\n");
-    else
-    {
-        if (nRemainder == 0)
-            printf("even.\n");
-        else if (nRemainder == 1)
-            printf("odd.\n");
-    }
+	if (nInput == 0)
+		printf("Unable to determine.\n");
+	else
+	{
+		if (nRemainder == 0)
+			printf("even.\n");
+		else if (nRemainder == 1)
+			printf("odd.\n");
+	}
 
 
 }
+
+
+//int main()
+//{
+//    int nInput{};
+//    int nRemainder{};
+//
+//    printf("Enter a Number.\n");
+//    scanf_s("%d", &nInput);
+//
+//    nRemainder = nInput % 2;
+//
+//    if (nInput == 0)
+//        printf("Unable to determine.\n");
+//    if (nInput != 0 && nRemainder == 0)
+//        printf("Even.\n");
+//    if (nInput != 0 && nRemainder == 1)
+//        printf("Odd.\n");
+//
+//}

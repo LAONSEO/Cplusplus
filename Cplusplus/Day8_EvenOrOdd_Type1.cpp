60e601c3c85936451115aa41e137f3cb3368251f
LAONSEO <gerard2725@gmail.com>
2022-11-21 23:44:15 +0900

    Day8_commit

---
+++ Cplusplus/Day8_EvenOrOdd_Type1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+int main()
+{
+    int nInput{};
+    int nRemainder{};
+
+    printf("Enter a Number.\n");
+    scanf_s("%d", &nInput);
+
+    nRemainder = nInput % 2;
+
+    if (nInput == 0)
+        printf("Unable to determine.\n");
+    else
+    {
+        if (nRemainder == 0)
+            printf("even.\n");
+        if (nRemainder == 1)
+            printf("odd.\n");
+    }
+
+
+}

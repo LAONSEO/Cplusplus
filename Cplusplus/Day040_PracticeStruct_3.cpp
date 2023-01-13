ac9f8844066736383af725e84cd142bc6a91c2cb
JEONGYUN SEO <gerard2725@gmail.com>
2023-01-13 23:59:26 +0900

    Day40_commit

---
+++ Cplusplus/Day040_PracticeStruct_3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct S_TEST
+{
+    int nData;
+    int* pData;
+};
+struct S_DATA
+{
+    int nData;
+    S_TEST* pTest;
+};
+
+int main()
+{
+    S_DATA* p;
+
+    (*(*p).pTest).nData;
+    p->pTest->nData;
+
+    *p->pTest->pData;
+}

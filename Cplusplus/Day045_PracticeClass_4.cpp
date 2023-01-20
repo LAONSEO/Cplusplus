f05bb24d153874c087b257a2348c0e5c1f0d6b5f
JEONGYUN SEO <gerard2725@gmail.com>
2023-01-20 15:29:57 +0900

    Day45_commit

---
+++ Cplusplus/Day045_PracticeClass_4.cpp
@@ -0,0 +1,21 @@
+// Day045_PracticeClass_4.cpp
+
+#include <iostream>
+
+class C_DATA
+{
+private:
+    int m_nData;
+
+public:
+    C_DATA(int);
+};
+
+int main()
+{
+    C_DATA c1(10);
+}
+
+C_DATA::C_DATA(int)
+{
+}

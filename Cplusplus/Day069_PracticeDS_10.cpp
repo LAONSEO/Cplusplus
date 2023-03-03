ef2bf6c0ac90976fc2609ae3db7f3320d9cc2f09
JEONGYUN SEO <gerard2725@gmail.com>
2023-03-04 00:30:00 +0900

    Day69_revision

--- Cplusplus/Day069_PracticeDS_10.cpp
+++ Cplusplus/Day069_PracticeDS_10.cpp
@@ -1,23 +1,25 @@
-// PracticeDS_10.cpp
+// PracticeDS_10.cpp // !!!!! wrong code.!!!!!
 
 #include <iostream>
 
-int SearchBinary(int* pData, int nLength, int value, int& nResult);
+bool SearchBinary(const int* pData, int nLength, int value, int& nResult);
 
 int main()
 {
 	int arData[7]{ 11,22,33,44,55,66,77 };
 	int nResult{};
 
-	SearchBinary(arData, 7, 22, nResult);
+	SearchBinary(arData, 7, 47, nResult);
 	printf("%d 번지에 있음. \n", nResult);
 }
 
-int SearchBinary(int* pData, int nLength, int value, int& nResult)
+bool SearchBinary(const int* pData, int nLength, int value, int& nResult)
 {
 	int nIndexStart = 0;
 	int nIndexEnd = nLength - 1;
 	int nIndexMid{};
+	int nCount1{};
+	int nCount2{};
 
 	nIndexMid = (nIndexStart + nIndexEnd) / 2;
 
@@ -28,17 +30,34 @@ int SearchBinary(int* pData, int nLength, int value, int& nResult)
 
 	while (value != pData[nIndexMid])
 	{
+		if (nCount1 > 0 && nCount2 > 0)
+		{
+			return false;
+		}
 		if (value < pData[nIndexMid])
 		{
 			nIndexEnd = nIndexMid - 1;
+			if (value > pData[nIndexMid - 1])
+			{
+				nResult = -1;
+				return false;
+			}
 			nIndexMid = (nIndexStart + nIndexEnd) / 2;
+			nCount1++;
+			nResult = nIndexMid;
 		}
 		if (value > pData[nIndexMid])
 		{
 			nIndexStart = nIndexMid + 1;
+			if (value < pData[nIndexMid + 1])
+			{
+				nResult = -1;
+				return false;
+			}
 			nIndexMid = (nIndexStart + nIndexEnd) / 2;
+			nCount2++;
+			nResult = nIndexMid;
 		}
-		nResult = nIndexMid;
 	}
-	return nResult;
+	return true;
 }

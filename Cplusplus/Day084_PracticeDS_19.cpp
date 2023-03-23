211d7a63fff7324770c4c7975f1e8bced7fc9b6d
JEONGYUN SEO <gerard2725@gmail.com>
2023-03-23 17:44:12 +0900

    Day84_commit

---
+++ Cplusplus/Day084_PracticeDS_19.cpp
@@ -0,0 +1,39 @@
+// PracticeDS_19.cpp : singleton.
+
+class PracticeDS_19_C_DATA
+{
+private:
+    int m_nData;
+
+private:
+    PracticeDS_19_C_DATA() = default;
+
+public:
+    void PracticeDS_19_C_DATA_setData(int nData);
+    int PracticeDS_19_C_DATA_getData();
+    static PracticeDS_19_C_DATA* PracticeDS_19_C_DATA_getInstance();
+};
+
+#include <iostream>
+
+int main()
+{
+    PracticeDS_19_C_DATA::PracticeDS_19_C_DATA_getInstance()->PracticeDS_19_C_DATA_setData(100);
+    printf("%d\n", PracticeDS_19_C_DATA::PracticeDS_19_C_DATA_getInstance()->PracticeDS_19_C_DATA_getData());
+}
+
+void PracticeDS_19_C_DATA::PracticeDS_19_C_DATA_setData(int nData)
+{
+    m_nData = nData;
+}
+
+int PracticeDS_19_C_DATA::PracticeDS_19_C_DATA_getData()
+{
+    return m_nData;
+}
+
+PracticeDS_19_C_DATA* PracticeDS_19_C_DATA::PracticeDS_19_C_DATA_getInstance()
+{
+    static PracticeDS_19_C_DATA cData{};
+    return &cData;
+}

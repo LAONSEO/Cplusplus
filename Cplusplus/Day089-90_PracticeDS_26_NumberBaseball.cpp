8b4a9ff8b09e9040e56c2ecf1f3379536eb7a55b
JEONGYUN SEO <gerard2725@gmail.com>
2023-03-31 17:00:25 +0900

    Day90_revision

--- Cplusplus/Day089-90_PracticeDS_26_NumberBaseball.cpp
+++ Cplusplus/Day089-90_PracticeDS_26_NumberBaseball.cpp
@@ -1,4 +1,4 @@
-// Day089_PracticeDS_26_NumberBaseball.
+// Day089-90_PracticeDS_26_NumberBaseball.
 // windows.h 적용을 위해 다음 절차 수행필요.
 // 속성 - 구성 속성 - 링커 - 입력 - 구성에서 활성debug, 활성(x64) 설정 - 추가종속성 최우측 편집 - winmm.lib 입력 후 적용.
 
